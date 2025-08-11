1396e2147341e9d18d11334dfd219741bd473c68
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-11 21:17:42 +0530

    refactor: clean up Header  by removing unused options dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,10 @@
-import {
-  LogOutIcon,
-  ReplyIcon,
-  SearchIcon,
-  SettingsIcon,
-  UserIcon,
-} from 'lucide-react';
+import { LogOutIcon, ReplyIcon, SearchIcon, UserIcon } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Input } from '@/components/ui/input';
@@ -78,26 +70,12 @@ export function Header() {
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end" className="w-56" forceMount>
-                <DropdownMenuLabel className="font-normal">
-                  <div className="flex flex-col space-y-1">
-                    <p className="font-medium text-sm leading-none">Name</p>
-                    <p className="text-muted-foreground text-xs leading-none">
-                      variar@gmail.com
-                    </p>
-                  </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
                 <DropdownMenuItem>
                   <UserIcon className="mr-2 h-4 w-4" />
                   <span>Profile</span>
                 </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <SettingsIcon className="mr-2 h-4 w-4" />
-                  <span>Settings</span>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <LogOutIcon className="mr-2 h-4 w-4" />
+                <DropdownMenuItem className="text-red-500">
+                  <LogOutIcon className="mr-2 h-4 w-4 text-red-500" />
                   <span>Log out</span>
                 </DropdownMenuItem>
               </DropdownMenuContent>
