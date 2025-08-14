b62ebffed35125bd565568405112d3461c9d209a
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-14 19:03:00 +0530

    feat(localization): add localization support for NotFound module

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,26 +1,26 @@
+import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
 import { PageLayout } from '@/components/Layout/PageLayout';
 import { Button } from '@/components/ui/button';
 
 export default function NotFoundPage() {
   const navigate = useNavigate();
+  const { t } = useTranslation();
   const goBack = () => {
     navigate(-1);
   };
   return (
     <PageLayout
       className="flex h-screen flex-col items-center justify-center text-center"
-      title="Not found"
+      title={t('notFoundPage.title')}
     >
       <h1 className="mb-4 font-bold text-6xl text-primary">404</h1>
       <h2 className="mb-2 font-semibold text-2xl text-gray-800">
-        Page Not Found
+        {t('notFoundPage.pageNotFound')}
       </h2>
-      <p className="mb-6 text-muted-foreground">
-        The page you are looking for does not exist or has been moved.
-      </p>
+      <p className="mb-6 text-muted-foreground">{t('notFoundPage.message')}</p>
       <Button className="" onClick={goBack} size="lg">
-        Go Back
+        {t('notFoundPage.goBackButton')}
       </Button>
     </PageLayout>
   );
